ef5cfc65cfa4b06b7920f6486a0d5e11415f0c41
Andrés Masegosa Arredondo <andresmasegosa@gmail.com>
2015-08-27 10:20:44 +0200

    Update

--- Index.mk
+++ Index.mk
@@ -10,7 +10,7 @@ Apache Software License 2.0.
 In the next figure we show a taxonomy of relevant data mining tools dealing with PGMs and data streams. To the best of our knowledge, there is no other software for mining data streams based on PGMs, most of the existing softwares based on PGMs are only focused on mining stationary data sets. Hence, the main goal of AMIDST is to fill this gap and produce a significant contribution within the areas of PGMs and mining streaming data.
 
 <p align="center">
-<img title="Taxonomy" src="https://amidst.github.io/toolbox/docs/taxonomy.pdf" width="500">
+<img title="Taxonomy" src="https://amidst.github.io/toolbox/docs/taxonomy.png" width="500">
 </p>
 
 # Scalability
@@ -23,11 +23,13 @@ a hybrid BN model with latent variables using the AMIDST's learning engine. More
 with multinomial (blue nodes) and Gaussian (green nodes) variables, some of them are latent, non observable, 
 variables (dashed nodes). As can be seen, using our variational learning engine AMIDST toolbox is able to 
 process data in the order of gigabytes per hour depending on the number of available cores with large and 
-complex PGMs with latent variables(*).
+complex PGMs with latent variables (these experiments were carried out on a Ubuntu Linux server with a x86_64 
+architecture and 32 cores. The size of the processed data set was measured according to the 
+[Weka](www.cs.waikato.ac.nz/ml/weka/)'s ARFF format).
 
 
 <p align="center">
-<img src="https://amidst.github.io/toolbox/docs/scalability.pdf" width="800">
+<img src="https://amidst.github.io/toolbox/docs/scalability.png" width="800">
 </p>
 
 
@@ -58,9 +60,6 @@ In the following repository it is hosted the software code of some use-cases and
 
 [https://github.com/amidst/toolbox-usecases](https://github.com/amidst/toolbox-usecases)
 
-
 ## Upcoming Developments
 
 AMIDST Toolbox is an expanding project and upcoming developments include the implementation of [dynamic models](https://en.wikipedia.org/wiki/Dynamic_Bayesian_network) for handling streaming data, the integration of the toolbox in Big Data platforms like [Spark](http://spark.apache.org) and [Flink](http://flink.apache.org) to enlarge its scalability capacities, and a new link to [R](http://static.amidst.eu/upload/dokumenter/Posters/PosterUseR.pdf) to expand the users base.
-
-(*)These experiments were carried out on a Ubuntu Linux server with a x86_64 architecture and 32 cores. The size of the processed data set was measured according to the [Weka](www.cs.waikato.ac.nz/ml/weka/)'s ARFF format.
